b60cebcab6af38bc5a256b89abb77857a3e09620
Breno Cunha Queiroz <breno1423@gmail.com>
2019-03-13 01:44:42 -0300

    Deleted some comments

--- SensorDataFirebase-Raspberry/escolaverde.py
+++ SensorDataFirebase-Raspberry/escolaverde.py
@@ -2,24 +2,13 @@ from firebase import firebase
 from Libraries.SensorFirebasePi import SensorFirebasePi
 import time
 
-sensorFirebase = SensorFirebasePi('https://escolaverde-febrace.firebaseio.com/')
+sensorFirebase = SensorFirebasePi('https://escolaverde-portinari.firebaseio.com/')
 
-sensorFirebase.addSensor('temperature','Sensors/Temperature')
-sensorFirebase.addSensor('humidity','Sensors/Humidity')
+sensorFirebase.addSensor('temperature','Sensors/Temperature/')
+sensorFirebase.addSensor('humidity','Sensors/Humidity/')
 sensorFirebase.sensorInfo('temperature')
 sensorFirebase.sensorInfo('humidity')
 
 while True:
 	sensorFirebase.run()
-	time.sleep(1) 
-
-'''
-sendFirebase.addSensor('temperature','Sensors/Temperature')
-
-sendFirebase.printSensor('temperature')
-
-firebase = firebase.FirebaseApplication('https://escolaverde-febrace.firebaseio.com/')
-result = firebase.put('Sensors/Humidity','00h30',22)
-getData = firebase.get('/Sensors/Humidity/00h00',None);
-print(result)
-print(getData)'''
+	time.sleep(1)
