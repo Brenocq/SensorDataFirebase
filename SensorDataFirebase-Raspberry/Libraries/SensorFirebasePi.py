5dd72d87cda04ef223374dd726db2b25d61bfe1a
Breno Cunha Queiroz <breno1423@gmail.com>
2019-03-13 01:48:24 -0300

    First time working to read data from bytes and write on the firebase
    -Reduced the baudrate from 115200 to 9600
    -Conversion from bytes to float
    -Added function to set sensor values for firebase "today" and "yesterday"

--- SensorDataFirebase-Raspberry/Libraries/SensorFirebasePi.py
+++ SensorDataFirebase-Raspberry/Libraries/SensorFirebasePi.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
+from firebase import firebase
 import serial
 import struct
 
-ser = serial.Serial("/dev/ttyAMA0",115200,timeout=1)
+ser = serial.Serial("/dev/ttyAMA0",9600,timeout=1)
 
 class SensorFirebasePi:
-    
+
     sensors    = {}
     sensorsName = ["None"]
     sensorsNum = 0
@@ -16,10 +17,12 @@ class SensorFirebasePi:
     day = 0
     month = 0
     year = 0
+    firebase = 0
 
     def __init__(self,linkFirebase):
         self.link = linkFirebase
- 
+        self.firebase = firebase.FirebaseApplication(self.link)
+
     def addSensor(self,name, address):
         self.sensorsNum += 1;
         self.sensors[name]= {'address':address, 'number': self.sensorsNum}
@@ -36,9 +39,9 @@ class SensorFirebasePi:
             print("\n")
 
     def run(self):
-        try:    
+        try:
             init1 = ord(ser.read())
-            init2 = ord(ser.read()) 
+            init2 = ord(ser.read())
             code1 = ord(ser.read())
             code2 = ord(ser.read())
             if(code1==0 and code2==0):
@@ -51,61 +54,177 @@ class SensorFirebasePi:
                 self.month = ord(ser.read())
                 self.year = ord(ser.read())+2018
                 sumBytes = ord(ser.read())
-                print("Receiving time: %d:%d:%d (DoW:%d) %d/%d/%d c:%d" % \
-                    (self.hour,self.minute,self.second,self.dayOfWeek,self.day,self.month,self.year,sumBytes))
-            else:
+
+                self.dayOfWeekZeller()#Update the dayOfWeek value using info about day,month and year
+
+                print("Receiving(time) - data: %d:%d:%d (DoW:%d) %d/%d/%d" % \
+                    (self.hour,self.minute,self.second,self.dayOfWeek,self.day,self.month,self.year))
+
+                #----- Print Cycle -----#
+                if(self.minute % 30 == 0):
+                    print("\n#--------------- CYCLE %dh%d ---------------#" % (self.hour,self.minute))
+                #----- Update Time Firebase -----#
+                hourStr      = ''
+                minuteStr    = ''
+                secondStr    = ''
+
+                if self.hour < 10:
+                    hourStr = '0' + str(self.hour)
+                else:
+                    hourStr = str(self.hour)
+
+                if self.minute < 10:
+                    minuteStr = '0' + str(self.minute)
+                else:
+                    minuteStr = str(self.minute)
+
+                if self.second < 10:
+                    secondStr = '0' + str(self.second)
+                else:
+                    secondStr = str(self.second)
+
+                currentTimeStr = hourStr + ":" + minuteStr + ":" + secondStr
+
+                print("SET firebase: Time/CurrentTime = %s" % (currentTimeStr))
+                self.firebase.put('Time','CurrentTime',currentTimeStr)
+
+                #----- Update Date Firebase -----#
+                dayStr      = ''
+                monthStr    = ''
+
+                if self.day < 10:
+                    dayStr = '0' + str(self.day)
+                else:
+                    dayStr = str(self.day)
+
+                if self.month < 10:
+                    monthStr = '0' + str(self.month)
+                else:
+                    monthStr = str(self.month)
+
+                currentDateStr = dayStr + "/" + monthStr + "/" +str(self.year)
+
+                print("SET firebase: Time/CurrentDate = %s" % (currentDateStr))
+                self.firebase.put('Time','CurrentDate',currentDateStr)
+            elif(code1 == code2):
                 #Receiving some sensor data
                 b1 = ord(ser.read())
                 b2 = ord(ser.read())
                 b3 = ord(ser.read())
                 b4 = ord(ser.read())
                 sumBytes = ord(ser.read())
-                
-                x = [hex(b1),hex(b2),hex(b3),hex(b4)]
-                y = int.from_bytes(x, byteorder='little', signed=False) #interpret bytes as an unsigned little-endian integer (so far so good)
-                z = float(y) #attempt to cast as float reinterprets integer value rather than its byte values
-                print z
-                '''
-                #data  = [b1, b2, b3, b4]
-                data  = [51,51,215,65]
-                b = ''.join(chr(i) for i in data)
-                print struct.unpack('>f', b)
-                '''
-                #value = struct.unpack('f', b"%s"%(byteString))
-                print("Receiving %s: (%d %d %d %d) c:%d" % (self.sensorsName[code1],b1,b2,b3,b4,sumBytes))  
-                #print(value)
+
+                byte1=format(struct.unpack('!I', struct.pack('!i', b1))[0], '08b')
+                byte2=format(struct.unpack('!I', struct.pack('!i', b2))[0], '08b')
+                byte3=format(struct.unpack('!I', struct.pack('!i', b3))[0], '08b')
+                byte4=format(struct.unpack('!I', struct.pack('!i', b4))[0], '08b')
+
+                floatData=struct.unpack('!f',struct.pack('!I', int(byte4+byte3+byte2+byte1, 2)))[0]
+
+                print("Receiving(%s) - bytes: %d %d %d %d - value: %f" % (self.sensorsName[code1],b1,b2,b3,b4,floatData))
+
+                #Checking sum received (confirmation byte)
+                sumReceived = code1 + b1 + b2 + b3 + b4
+                while (sumReceived>255):
+                    sumReceived -= 255
+
+                if(sumReceived == sumBytes):
+                    print("#----- Sensor %s -----#" % (self.sensorsName[code1]))
+                    if (self.hour == 0 and self.minute < 10):
+                        self.updateFirebaseYesterday(self.sensorsName[code1])#				UPDATE YESTERDAY
+
+                    self.updateFirebaseToday(self.sensorsName[code1], floatData)#		UPDATE DAY
+
+                    if (self.hour == 23 and self.minute >= 30 and self.minute < 40):
+                        self.updateFirebaseMonth(self.sensorsName[code1])#					UPDATE MONTH
+                        if (self.dayOfWeek == 0):#Sunday
+                            self.updateFirebaseYear(self.sensorsName[code1])#				UPDATE YEAR
+                else:
+                    print("ERROR: wrong confirmation byte (%d != %d)" % (sumReceived,sumBytes))
+            else:
+                print("ERROR: code1 != code2 (%d != %d)" % (code1,code2))
+
         except:
             pass
-
-    def updateFirebaseToday(self,name):
-        print('update today')
-
+#**********************************************************************************************************************TODAY
+    def updateFirebaseToday(self,name,value):
+        print('#TODAY#')
+        indexHour = (self.hour * 2) + (self.minute / 30)
+        #each index refer to one hour
+        hoursStr= [  "00h00", "00h30", "01h00", "01h30", "02h00", "02h30", "03h00", "03h30", "04h00", "04h30",\
+        			 "05h00", "05h30", "06h00", "06h30", "07h00", "07h30", "08h00", "08h30", "09h00", "09h30",\
+        			 "10h00", "10h30", "11h00", "11h30", "12h00", "12h30", "13h00", "13h30", "14h00", "14h30",\
+        			 "15h00", "15h30", "16h00", "16h30", "17h00", "17h30", "18h00", "18h30", "19h00", "19h30",\
+        			 "20h00", "20h30", "21h00", "21h30", "22h00", "22h30", "23h00", "23h30"]
+        #create complete address to firebase
+        dataBaseAddress = self.sensors[name]['address'] + "Today/"
+        #print address screen
+        print("SET firebase: %s = %f" % (dataBaseAddress,value))
+        #set value firebase
+        self.firebase.put(self.sensors[name]['address'] + "Today/",hoursStr[indexHour], value)
+        print('#END#')
+#**********************************************************************************************************************YESTERDAY
     def updateFirebaseYesterday(self,name):
-        print('update today')
+        print('#YESTERDAY#')
+        #----- get values Today -----#
+        #each index refer to one hour
+        hoursStr = [ "00h00", "00h30", "01h00", "01h30", "02h00", "02h30", "03h00", "03h30", "04h00", "04h30",\
+        		"05h00", "05h30", "06h00", "06h30", "07h00", "07h30", "08h00", "08h30", "09h00", "09h30",\
+        		"10h00", "10h30", "11h00", "11h30", "12h00", "12h30", "13h00", "13h30", "14h00", "14h30",\
+        		"15h00", "15h30", "16h00", "16h30", "17h00", "17h30", "18h00", "18h30", "19h00", "19h30",\
+        		"20h00", "20h30", "21h00", "21h30", "22h00", "22h30", "23h00", "23h30" ]
 
+        valuesToday = []
+
+        for i in range(48):
+        	#get all values from Today (files hours[i] inside folder)
+            dataBaseAddress = self.sensors[name]['address'] + "Today/" + hoursStr[i]
+            valuesToday.append(self.firebase.get(dataBaseAddress,None))
+            #print valuesToday[i]
+            #valuesToday.append(self.firebase.get(dataBaseAddress,None))
+        	#print address screen
+            if valuesToday[i] != None:
+                print("GET firebase: %s : %f" % (dataBaseAddress,valuesToday[i]))
+            else:
+                print("GET firebase: %s : None" % (dataBaseAddress))
+
+        #----- update values Yesterday -----#
+        dataBaseAddressYest = self.sensors[name]['address'] + "Yesterday/"
+        print("DELETE firebase: %s" % (dataBaseAddressYest))
+        self.firebase.delete(dataBaseAddressYest, None)
+
+        for i in range(48):
+            if valuesToday[i] != None:
+                print(i)
+                print("SET firebase: %s = %f" % (dataBaseAddressYest+hoursStr[i],valuesToday[i]))
+                self.firebase.put(dataBaseAddressYest,hoursStr[i], valuesToday[i])
+
+        #----- delete values Today -----#
+        #create complete address to firebase
+        dataBaseAddressToday = self.sensors[name]['address'] + "Today"
+        #print address screen
+        print("DELETE firebase: %s" % (dataBaseAddressToday))
+        #remove value firebase
+        self.firebase.delete(dataBaseAddressToday, None)
+
+        print('#END#')
+#**********************************************************************************************************************MONTH
     def updateFirebaseMonth(self,name):
-        print('update today')
+        #TODO finish updateFirebaseMonth
+        print('update Month')
 
+#**********************************************************************************************************************YEAR
     def updateFirebaseYear(self,name):
-        print('update today')
+        #TODO finish updateFirebaseMonth
+        print('update Year')
 
-    def dayOfWeekZeller(self, day, month, year):
-        k = day
-        m = month + 10 #"March is 1, April is 2, and so on to February, which is 12"
-        m > 12 ? m -=12 : m;
-        D = year - 2000;#vai dar bug em 2100 
-        C = int(year / 100)
-        dayOfWeek = k + ((13 * m - 1) / 5) + D + (D / 4) + (C / 4) - 2 * C
-        return dayOfWeek >= 7 ? dayOfWeek = dayOfWeek % 7 : dayOfWeek
-
-'''
-    _sensorNum = 0
-    def __init__(self,linkFirebase):
-        self.link = linkFirebase
-    
-#   def run(self):
-
-    def printSensor(self,name):
-        print("Name: ",name)
-        print("Number: ",self.__dict__[name]._number)
-        print("Address: ",self.__dict__[name].address)'''
+    def dayOfWeekZeller(self):
+        k = self.day
+        m = self.month + 10 #"March is 1, April is 2, and so on to February, which is 12"
+        if m > 12:
+            m -=12
+        D = self.year - 2000;#vai dar bug em 2100
+        C = int(self.year / 100)
+        self.dayOfWeek = k + ((13 * m - 1) / 5) + D + (D / 4) + (C / 4) - 2 * C
+        if self.dayOfWeek >= 7:
+            self.dayOfWeek = self.dayOfWeek % 7
